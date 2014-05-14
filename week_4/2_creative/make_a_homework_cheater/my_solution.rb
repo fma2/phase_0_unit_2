b44d575b3c8040e2758dcb6be554ce904f6390ca
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-13 22:11:02 -0400

    Began to write pseudocode

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -5,9 +5,13 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: parameters list with the following parameters: title, person/place/topic, date, thesis statement.  Parameters are plugged into a template
+# Output: template with parameters plugged in, in the form of story
 # Steps:
+
+# Create method "essay_writer"
+# Identify parameters of method: title, person, pronoun, occupation, year, city, thesis_statement
+# 
 
 
 # 3. Initial Solution
@@ -27,7 +31,8 @@
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
 
-
+# # (person) hails from (city).  (pronoun) has been a (occupation) since (year).  (thesis_statement)
+# example madlibs on bands: http://www.semeronmusic.com/2013/05/22/band-bio-mad-libs/
 
 
 
