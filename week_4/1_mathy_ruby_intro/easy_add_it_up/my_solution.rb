e59f029bc9c9fc1cf38fe82e0964eb5d89e288f2
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-13 20:38:32 -0400

    Update with pseudocode

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,18 +4,36 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
 # What is the input?
+
+# For finding the sum of the array, the input is each number in the array.
+# For creating a sentence out of the array, the input is each string in the array.
+
 # What is the output? (i.e. What should the code return?)
+
+# For finding the sum of the array, the output is one number, the total of all the numbers in the array added together.
+# For creating a sentence out of the array, the output is a single string (sentence) with all of the strings joined.
+
 # What are the steps needed to solve the problem?
 
+# FINDING THE SUM FROM ARRAY
+# Create a method "total" with parameter "array"
+# Establish sum as 0
+# Use .inject method on array
+# In .inject method, establish 0 as initial 
+# Define accumulator value as sum & element as i 
+# End
+
+# CREATING SENTENCE FROM ARRAY
+# Create a method "sentence_maker" with parameter "array"
+# Use .join method on array
+# End
 
 # 2. Initial Solution
-
-
 
 
 # 3. Refactored Solution
