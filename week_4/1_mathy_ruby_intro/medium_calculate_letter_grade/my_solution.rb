ec032f0c5c45ae19ac07b1195446995a3a2fc759
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-13 21:39:23 -0400

    Add pseudocode

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -9,8 +9,22 @@
 # 1. Pseudocode
 
 # What is the input?
+
+# A method with parameter array in which there will be an array of scores
+
 # What is the output? (i.e. What should the code return?)
+
+# The letter grade
+
 # What are the steps needed to solve the problem?
+
+# Create a method "get_grade" which takes "array" as the parameter
+# Create variable to sum all of the scores
+# Create variable "y" to divide the sum by the total number of scores
+# Create if/then statements, if y >= 90, return "A" .. and so on
+# End if/then method
+# End "get_grade" method
+
 
 
 # 2. Initial Solution
@@ -19,6 +33,7 @@
 
 
 # 3. Refactored Solution
+
 
 
 
