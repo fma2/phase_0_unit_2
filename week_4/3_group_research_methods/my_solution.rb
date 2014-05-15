fe89b76f1171f64c8c184b6b99e29d7ac731e974
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-14 22:05:33 -0400

    Update my_solution - Person 1

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -1,65 +1,83 @@
 # U2.W4: Research Methods
 
-i_want_pets = ["I", "want", 3, "pets", "but", "I", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, 
             "Annabelle" => 0, "Ditto" => 3}
+i_want_pets = ["I", "want", 3, "pets", "but", "I", "only", "have", 2]
 
 # Person 1's solution
-def my_array_finding_method(source, thing_to_find)
-  # Your code here!
-end
 
-def my_hash_finding_method(source, thing_to_find)
-  # Your code here!
-end
+# PSEUDOCODE
+# Create a method my_array_finding_method that takes an Array and a letter as arguments and returns the words that contain that letter.
+# Add parameters for method "array" and "letter"
+# Use method #delete_if to delete elements in array that are not a string
+# Use method #delete_if to delete elements in array that do not have the letter, use #match method
 
-# Identify and describe the ruby method you implemented. 
-# 
-#
-#
+def my_array_finding_method(array, letter)
+	array.delete_if {|word| word.class != String}
+  	array.delete_if {|word| word.match(letter) == nil }
+  	return array
+ end
 
-# Person 2
-def my_array_modification_method(source, thing_to_modify)
-  # Your code here!
-end
+# PSEUDOCODE
+# Create a method my_hash_finding_method that takes a Hash and number as arguments and returns a list of pet names by age. 
+# Add parameters to method "hash" and "number"
+# Sort numbers in order
+# Lists pets in order by age
 
-def my_hash_modification_method(source, thing_to_modify)
-  # Your code here!
-end
-
-# Identify and describe the ruby method you implemented. 
-# 
-#
-#
-
-
-# Person 3
-def my_array_sorting_method(source)
-  # Your code here!
-end
-
-def my_hash_sorting_method(source)
-  # Your code here!
+def my_hash_finding_method(hash, number)
+	hash.delete_if {|key, value| value != number }
+	hash.keys
 end
 
 # Identify and describe the ruby method you implemented. 
-# 
+# For my_array_finding_method, I utilized the method delete_if, which deletes every element of the array for which block evaluates to true.  I ran the method twice -- once to delete the numbers in the array and another time to delete the words that did have the letter.
 #
+# For my_hash_finding_method, I utilized the method delete_if which deletes every key-value pair from the hash for which block evaluates to true.
+# I then extracted the keys from the hash.
 #
 
+# # Person 2
+# def my_array_modification_method(source, thing_to_modify)
+#   # Your code here!
+# end
 
-# Person 4
-def my_array_deletion_method(source, thing_to_delete)
-  #Your code here!
-end
+# def my_hash_modification_method(source, thing_to_modify)
+#   # Your code here!
+# end
 
-def my_hash_deletion_method(source, thing_to_delete)
-  #Your code here!
-end
+# # Identify and describe the ruby method you implemented. 
+# # 
+# #
+# #
 
-# Identify and describe the ruby method you implemented. 
-# 
-#
+
+# # Person 3
+# def my_array_sorting_method(source)
+#   # Your code here!
+# end
+
+# def my_hash_sorting_method(source)
+#   # Your code here!
+# end
+
+# # Identify and describe the ruby method you implemented. 
+# # 
+# #
+# #
+
+
+# # Person 4
+# def my_array_deletion_method(source, thing_to_delete)
+#   #Your code here!
+# end
+
+# def my_hash_deletion_method(source, thing_to_delete)
+#   #Your code here!
+# end
+
+# # Identify and describe the ruby method you implemented. 
+# # 
+# #
 #
 
 
