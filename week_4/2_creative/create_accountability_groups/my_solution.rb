eb36902a5eecdd2e045efbff07c57efc885057c8
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-17 15:59:29 -0400

    Updates to initial and refactored code

--- week_4/2_creative/create_accountability_groups/my_solution.rb
+++ week_4/2_creative/create_accountability_groups/my_solution.rb
@@ -21,57 +21,57 @@
 
 # 3. Initial Solution
 
-# dbc_newts = "Rootul Patel","Hilary Barr", "Alan Florendo", "Cassie Moy", "Stephen Craig Estrada", "Austin Hay", "Anthony Edwards Jr.", "John Berry", "Farheen Malik","Daniel Deepak", "RJ Arena", "Justin Lee", "Michael Weiss", "David Sin", "Julius Jung", "Fahia Mohamed", "Molly Huerster", "Eric Hou", "Avi Fox-Rosen", "Joel Yawili", "Dylan Richards", "Kaitlyn La", "Derek Siker", "Dylan Krause"
+dbc_newts = "Rootul Patel","Hilary Barr", "Alan Florendo", "Cassie Moy", "Stephen Craig Estrada", "Austin Hay", "Anthony Edwards Jr.", "John Berry", "Farheen Malik","Daniel Deepak", "RJ Arena", "Justin Lee", "Michael Weiss", "David Sin", "Julius Jung", "Fahia Mohamed", "Molly Huerster", "Eric Hou", "Avi Fox-Rosen", "Joel Yawili", "Dylan Richards", "Kaitlyn La", "Derek Siker", "Dylan Krause"
 
-# dbc_group1 = dbc_newts.shuffle.slice(0,4)
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group1[0])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group1[1])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group1[2])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group1[3])}
+dbc_group1 = dbc_newts.shuffle.slice(0,4)
+		dbc_newts.delete_if {|name| name.include?(dbc_group1[0])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group1[1])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group1[2])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group1[3])}
 
 
-# dbc_group2 = dbc_newts.shuffle.slice(0,4)
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group2[0])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group2[1])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group2[2])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group2[3])}
+dbc_group2 = dbc_newts.shuffle.slice(0,4)
+		dbc_newts.delete_if {|name| name.include?(dbc_group2[0])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group2[1])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group2[2])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group2[3])}
 
-# dbc_group3 = dbc_newts.shuffle.slice(0,4)
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group3[0])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group3[1])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group3[2])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group3[3])}
+dbc_group3 = dbc_newts.shuffle.slice(0,4)
+		dbc_newts.delete_if {|name| name.include?(dbc_group3[0])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group3[1])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group3[2])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group3[3])}
 
-# dbc_group4 = dbc_newts.shuffle.slice(0,4)
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group4[0])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group4[1])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group4[2])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group4[3])}
+dbc_group4 = dbc_newts.shuffle.slice(0,4)
+		dbc_newts.delete_if {|name| name.include?(dbc_group4[0])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group4[1])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group4[2])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group4[3])}
 
-# dbc_group5 = dbc_newts.shuffle.slice(0,4)
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group5[0])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group5[1])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group5[2])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group5[3])}
+dbc_group5 = dbc_newts.shuffle.slice(0,4)
+		dbc_newts.delete_if {|name| name.include?(dbc_group5[0])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group5[1])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group5[2])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group5[3])}
 
-# dbc_group6 = dbc_newts.shuffle.slice(0,4)
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group6[0])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group6[1])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group6[2])}
-# 		dbc_newts.delete_if {|name| name.include?(dbc_group6[3])}
+dbc_group6 = dbc_newts.shuffle.slice(0,4)
+		dbc_newts.delete_if {|name| name.include?(dbc_group6[0])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group6[1])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group6[2])}
+		dbc_newts.delete_if {|name| name.include?(dbc_group6[3])}
 	
-# print dbc_group1
-# print "--"
-# print dbc_group2
-# print "--"
-# print dbc_group3
-# print "--"
-# print dbc_group4
-# print "--"
-# print dbc_group5
-# print "--"
-# print dbc_group6
-# print "--"
+print dbc_group1
+print "--"
+print dbc_group2
+print "--"
+print dbc_group3
+print "--"
+print dbc_group4
+print "--"
+print dbc_group5
+print "--"
+print dbc_group6
+print "--"
 
 #With the above initial solution, you would have to run it 3 times to print groups for 3 units
 
