2bd2a0d573d23aef43a75fc6f627fe40902cf856
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-20 00:17:43 -0400

    Update soln w/pseudocode, initial, & refactored

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,41 +1,74 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
 # Input:
-# Output:
-# Steps:
+# class Die with argument sides
 
+# Output:
+# Number of sides
+# Number rolled
+
+# Steps:
+# Within initialize method, define instance variable @sides
+# Raise ArgumentError for when sides are less than 1
+# End method
+#
+# Within sides method, return instance variable sides
+# End method
+#
+# Within variable roll, return random number between 1 and number of sides
+# End method
+#
+#End class
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+    @sides = sides
+    raise ArgumentError, 'Argument is less than 1' if sides < 1 
   end
   
   def sides
-    # code goes here
+  	@sides
   end
   
   def roll
-    # code goes here
+    rand(1..sides)
+  end
+end
+
+die = Die.new(6)
+die.sides # returns 6
+die.roll # returns a random number between 1 and 6
+
+# 4. Refactored Solution
+
+class Die
+  def initialize(sides)
+    @sides = sides
+    raise ArgumentError, 'Argument is less than 1' if sides < 1 
+  end
+  
+  def sides
+  	@sides
+  end
+  
+  def roll
+    rand(1..sides)
   end
 end
 
 
-
-# 4. Refactored Solution
-
-
-
-
-
-
 # 1. DRIVER TESTS GO BELOW THIS LINE
+die = Die.new(200)
+	p die.sides == 200
+p die.roll != 0
+p die.roll >= 1
 
 
 
