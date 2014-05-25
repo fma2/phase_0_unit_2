4348d2da01b8d2f64985af5cb9ecbc9b8361d2d5
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-25 09:48:26 -0400

    Update with reflections

--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -82,3 +82,10 @@ die = Die.new([random_letter])
 
 
 # 5. Reflection 
+
+# Like the first challenge, I appreciated that I had researched classes in depth prior to attempting this challenge.  In the same vein, I took time to consider the differences between this die and the first challenge’s die before beginning to pseudocode.  This challenge also required “raise” which I have added to my list of “what to learn about when I get the time to do so”--I was able to include it as instructed in both challenges but would not know exactly how to apply it more widely in the future.   
+
+# Further, how driver code was expected to look for the purposes of this unit & week in phase 0 finally clicked for me in this challenge.  Previously, I thought I needed to come up with something new for the driver code; for some reason, I had a hard time accepting that I could just use the content of the provided spec file.  I realized this was possible just because I arbitrarily chose to try it.
+
+# This challenge had me use #length and #sample methods.  I have used both in prior challenges, but it was good to practice with them again. 
+
