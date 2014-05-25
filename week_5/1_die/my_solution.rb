44a979b2a54c951910074df7fcd34f9888f63799
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-25 13:22:47 -0400

    Update formatting of reflection

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -78,12 +78,24 @@ die = Die.new(200)
 
 
 # 5. Reflection 
-=begin
-# Before attempting this challenge, I researched classes.  I revisited the list resources provided last week on Ruby, watched available videos, and read and reread the provided Rubylearning.com article.  This strategy worked -- it led me to begin the challenge without hesitation.  I find that, when I am not sure what is possible with the language I must use, I am more reluctant to attempt to write the code.  Instead, in those cases, I find myself using Google to ask what is possible while I’m trying to write the code.  This can make the process incredibly frustrating at times; I am glad I was able to avoid this for this first challenge for this week.
+# Before attempting this challenge, I researched classes.  I revisited the list resources 
+# provided last week on Ruby, watched available videos, and read and reread the provided 
+# Rubylearning.com article.  This strategy worked -- it led me to begin the challenge without 
+# hesitation.  I find that, when I am not sure what is possible with the language I must use, I
+# am more reluctant to attempt to write the code.  Instead, in those cases, I find myself 
+# using Google to ask what is possible while I’m trying to write the code.  This can make the 
+# process incredibly frustrating at times; I am glad I was able to avoid this for this first 
+# challenge for this week.
 
-# Unfortunately, even with research, I still did not initially place “raise ArgumentError” in the correct section of my code.  I need to research “raise” to understand the full scope of how and when it is used.
+# Unfortunately, even with research, I still did not initially place “raise ArgumentError” 
+# in the correct section of my code.  I need to research “raise” to understand the full scope 
+# of how and when it is used.
 
-# I learned about the method rand( ) through this challenge.  The number of methods available in Ruby for strings, arrays, hashes, etc. still amazes me.  
+# I learned about the method rand( ) through this challenge.  The number of methods 
+# available in Ruby for strings, arrays, hashes, etc. still amazes me.  
 
-# In regards to Learning Competencies, I was able to implement a basic ruby class and identify when to use instance variables.  I need to further research how instance variables differ from class variables--what are examples for using each?  This challenge was the beginning of understanding how instance variables and methods represent the characteristics and actions of an object.
-=end
+# In regards to Learning Competencies, I was able to implement a basic ruby class and 
+# identify when to use instance variables.  I need to further research how instance variables 
+# differ from class variables--what are examples for using each?  This challenge was the 
+# beginning of understanding how instance variables and methods represent the characteristics 
+# and actions of an object.
