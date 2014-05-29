a853422bb9fce50cbd541fa682383e8e4732d844
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-29 01:05:55 -0400

    Update initial solution - not finished

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -5,11 +5,74 @@
 
 
 # Pseudocode
+# Define class Song, takes 2 arguments (???)
+	# Define method initalize, takes 2 arguments: song, artist
+	# Define instance variables: @song1, @song2
+	# End method
+
+# Define class Playlist, takes 3 arguments (???)
+	# Define method initialize, takes 3 arguments: songlist1, songlist2, songlist3
+	# Define instance variables
+	# End method
+
+
 
 
 
 # Initial Solution
+class Song
+	attr_reader :song_title, :artist
 
+	def initialize(song_title,artist)
+		@song_title = song_title
+		@artist = artist
+	end
+
+	def create_song
+		return "#{song_title} by #{artist}"
+	end
+
+	def play
+		return "#{song_title} by #{artist} is playing."
+	end
+
+end
+
+class Playlist
+	
+	# @@mixtape = []
+
+	def initialize(*songs)
+		@songs = *songs
+		@mixtape = []
+		@mixtape << songs.each {|song| song} ## NEED TO EDIT
+	end
+
+	def add(*songs)
+		@mixtape << songs
+	end
+
+	def num_of_tracks
+		@mixtape.length
+	end
+
+	def remove(song)
+		@mixtape.delete(song)
+	end
+
+	def includes?(song)
+		@mixtape.include?(song)
+	end
+
+	def play_all
+		@mixtape
+	end
+
+	def display
+		@mixtape.each {|song| puts "- " + song.to_s }
+	end
+
+end
 
 
 
