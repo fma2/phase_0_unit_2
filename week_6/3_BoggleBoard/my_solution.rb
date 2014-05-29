b2573e8abe75274532192d8b090055e1306c0d80
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-28 23:25:25 -0400

    Update with pseudocode, initial sln, and refactor

--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -5,16 +5,77 @@
 
 
 # 2. Pseudocode
+# Set Class
 
+# Define method initialize, method takes argument(board)
+ 
+# Define method create_word
+# Use .map to iterate 
 
+# Define method get_row, method takes 2 arguments
+# Return row
+
+# Define method get_column, method takes 2 arguments
+# Use .map method to iterate row and column
 
 # 3. Initial Solution
+# class BoggleBoard
+#   attr_reader :board, :row, :column_number
+ 
+#   def initialize(board)
+#       @board = board
+#   end
+  
+#   def get_row(row)
+#       @row = row
+#       @board[row]
+#   end
+  
+#   def get_column(column_number)
+#       board.map {|element| element[column_number]}
+#   end
+  
+#    def create_word(*coords)
+#     coords.map { |coord| board[coord.first][coord.last]}.join("")
+#   end
+ 
+# end
+ 
+# dice_grid = [["b", "r", "a", "e"],
+#              ["i", "o", "d", "t"],
+#              ["e", "c", "l", "r"],
+#              ["t", "a", "k", "e"]]
+ 
+# boggle_board = BoggleBoard.new(dice_grid)
+ 
+
+
+# 4. Refactored Solution
 class BoggleBoard
+  attr_reader :board, :row, :column_number
  
-  #your code here
- 
+  def initialize(board)
+      @board = board
+  end
+  
+  def get_row(row)
+      board[row]
+  end
+  
+  def get_column(column_number)
+      board.map {|element| element[column_number]}
+  end
+  
+   def create_word(*coords)
+    coords.map { |coord| board[coord.first][coord.last]}.join("")
+  end
+
+   # Optimize Your Learning
+  def get_diagonal(*coords)
+    coords.map { |coord| board[coord.first][coord.last]}
+ 	end
+ 	
 end
- 
  
 dice_grid = [["b", "r", "a", "e"],
              ["i", "o", "d", "t"],
@@ -22,19 +83,21 @@ dice_grid = [["b", "r", "a", "e"],
              ["t", "a", "k", "e"]]
  
 boggle_board = BoggleBoard.new(dice_grid)
- 
-
-
-# 4. Refactored Solution
-
-
-
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # create driver test code to retrieve a value at a coordinate here:
 # implement tests for each of the methods here:
+boggle_board_test = BoggleBoard.new(dice_grid)
+p boggle_board_test.get_row(0) == ["b", "r", "a", "e"]
+p boggle_board_test.get_column(1) == ["r", "o", "c", "a"]
+
+p boggle_board_test.create_word([1,2],[1,1],[2,1],[3,2]) == "dock"
+
+p boggle_board_test.create_word([3,2]) == "k"
+
+p boggle_board_test.get_diagonal([0,0], [1,1], [2,2], [3,3]) == ["b","o","l","e"]
 
 
 # 5. Reflection 
