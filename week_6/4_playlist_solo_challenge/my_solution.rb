8d71c7d722092b63fdd224edb064ae644088fef4
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-29 20:37:28 -0400

    Update with pseudocode, initial, and refactored

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -5,31 +5,111 @@
 
 
 # Pseudocode
-# Define class Song, takes 2 arguments (???)
-	# Define method initalize, takes 2 arguments: song, artist
-	# Define instance variables: @song1, @song2
+# Define class Song
+	# Define method initalize, takes 2 arguments: song_artist, artist
+	# Define instance @song_title and @artist
 	# End method
+
+	# Define method create_song
+	# Return string
+	# End method
+
+	# Define method play
+	# Return string
+	# End method
+
+# End class
 
 # Define class Playlist, takes 3 arguments (???)
-	# Define method initialize, takes 3 arguments: songlist1, songlist2, songlist3
-	# Define instance variables
+	# Define method initialize, takes arguments *songs
+	# Define instance variable @songs = *songs
+	# Define @mixtape = []
+	# Iterate over songs to add each to @mixtape
 	# End method
 
+	# Define method add, takes arguments *songs
+	# Iterate over songs to add each to @mixtape
+	# End method
 
+	# Define method num_of_tracks
+	# Use method .length with @mixtape
+	# End method
 
+	# Define method remove, take argument song
+	# Use method .delete with song
+	# End method
 
+	# Define method includes, take argument song
+
+	# Define method play_all
+	# Return @mixtape
+	# End method
+
+	# Define method display
+	# Iterate over @mixtape to return each song
+	# End method
 
 # Initial Solution
+# class Song
+# 	attr_reader :song_title, :artist
+
+# 	def initialize(song_title,artist)
+# 		@song_title = song_title
+# 		@artist = artist
+# 	end
+
+# 	def create_song
+# 		return "#{song_title} by #{artist}"
+# 	end
+
+# 	def play
+# 		return "#{song_title} by #{artist} is playing."
+# 	end
+
+# end
+
+# class Playlist
+# 	attr_reader :songs
+
+# 	def initialize(*songs)
+# 		@songs = *songs
+# 		@mixtape = []
+# 		songs.each { |song| @mixtape << song }
+# 	end
+
+# 	def add(*songs)
+# 		songs.each { |song| @mixtape << song }
+# 	end
+
+# 	def num_of_tracks
+# 		@mixtape.length
+# 	end
+
+# 	def remove(song)
+# 		@mixtape.delete(song)
+# 	end
+
+# 	def includes?(song)
+# 		@mixtape.include?(song)
+# 	end
+
+# 	def play_all
+# 		@mixtape
+# 	end
+
+# 	def display
+# 		@mixtape.each {|song| "- " + song.to_s }
+# 	end
+
+# end
+
+# Refactored Solution
 class Song
 	attr_reader :song_title, :artist
 
 	def initialize(song_title,artist)
-		@song_title = song_title
-		@artist = artist
-	end
-
-	def create_song
-		return "#{song_title} by #{artist}"
+		song_title = song_title
+		artist = artist
 	end
 
 	def play
@@ -39,48 +119,39 @@ class Song
 end
 
 class Playlist
-	
-	# @@mixtape = []
+	attr_reader :songs, :mixtape
 
 	def initialize(*songs)
-		@songs = *songs
+		songs = *songs
 		@mixtape = []
-		@mixtape << songs.each {|song| song} ## NEED TO EDIT
+		songs.each { |song| mixtape << song }
 	end
 
 	def add(*songs)
-		@mixtape << songs
+		songs.each { |song| mixtape << song }
 	end
 
 	def num_of_tracks
-		@mixtape.length
+		mixtape.length
 	end
 
 	def remove(song)
-		@mixtape.delete(song)
+		mixtape.delete(song)
 	end
 
 	def includes?(song)
-		@mixtape.include?(song)
+		mixtape.include?(song)
 	end
 
 	def play_all
-		@mixtape
+		mixtape
 	end
 
 	def display
-		@mixtape.each {|song| puts "- " + song.to_s }
+		mixtape.each {|song| "- " + song.to_s }
 	end
 
 end
-
-
-
-# Refactored Solution
-
-
-
-
 
 
 # DRIVER TESTS GO BELOW THIS LINE
