0d6bda5825400afc8235f251dfda28832f179693
Farheen Malik <farheen.m.malik@gmail.com>
2014-05-31 12:58:35 -0400

    Add some solution

--- week_6/8_BONUS_CarClass/my_solution.rb
+++ week_6/8_BONUS_CarClass/my_solution.rb
@@ -6,22 +6,220 @@
 
 # 2. Pseudocode
 
+# class Car
+
+  # INITIALIZE car(model, color)
+    # @distance = 0
+  # END
+
+  # DEFINE drive(miles, speed)
+    # @distance = @distance + miles
+    # @speed = speed
+    
+  # DEFINE speed
+    # RETURN @speed
+    # END
+    
+  # DEFINE turn_at_stop_sign(direction)
+    # if left 
+      # turn left
+    # else 
+      # turn right
+    # END
+  
+  # DEFINE change_speed(amount_change)
+    # @speed = @speed + amount_change
+  
+  # DEFINE total_distance
+    # RETURN @distance
+  
+  # DEFINE stop
+    # speed = 0
+  
+  # DEFINE output(action = @array.pop)
+    # RETURN array.pop
+    # @speed
+    # @distance
 
 
 # 3. Initial Solution
 
 
+=begin
+class Car
+  
+  def initialize(model, color)
+    @model = model
+    @color = color
+  end
+  
+end
 
+class Directions
+  
+  def initialize(car)
+    @car = car
+    @distance = 0
+  end
+  
+  def drive(miles, speed)
+    @distance = @distance + miles
+    @speed = speed
+  end
+  
+  def speed
+    @speed
+  end
+  
+  def turn_at_stop(direction)
+    @directions
+  end
+  
+  def change_speed(amount_change)
+    @speed = @speed + amount_change
+  end
+  
+  def total_distance
+    @distance
+  end
+  
+  def stop
+    @speed = 0
+  end
+  
+  def output(action = array.pop)
+    action
+    @distance
+    @speed
+  end
+  
+end
+=end
 
 # 4. Refactored Solution
 
+class Car
+  
+  def initialize(model, color, *pizzas)
+    @model = model
+    @color = color
+    @insulated_compartment = []
+    @pizzas = *pizzas
+    @pizzas.each { |pizza| @insulated_compartment << pizza }
+  end
+  
+  def add_pizza(*pizzas)
+    @pizzas = *pizzas
+    @pizzas.each { |pizza| @insulated_compartment << pizza }
+  end
+  
+  def get_pizza(pizza = @insulated_compartment.first)
+    @insulated_compartment.delete(pizza)
+    "Here is your #{pizza.type} pizza."
+  end
+  
+  def see_all_pizza
+    puts "The insulated compartment contains:"
+    @insulated_compartment.each {|pizza| puts "- " + pizza.type }
+  end
+  
+  def pizza_count
+    @insulated_compartment.count
+    "You have #{@insulated_compartment.count} pizza(s) left in your insulated compartment."
+  end
+  
+end
 
+class Directions
+  
+  def initialize(car)
+    @car = car
+    @distance = 0
+  end
+  
+  def drive(miles, speed)
+    @distance = @distance + miles
+    @speed = speed
+    return "You drove your car #{@distance} miles at #{@speed} MPH."
+  end
+  
+  def speed
+    return "You are driving #{@speed} MPH."
+  end
+  
+  def turn_at_stop(direction)
+    return "You stopped at a stop sign and turned #{direction}."
+  end
+  
+  def change_speed(amount_change)
+    @speed = @speed + amount_change
+    return "You changed your speed to #{@speed} MPH."
+  end
+  
+  def total_distance
+    return "You have traveled #{@distance} miles."
+  end
+  
+  def stop
+    @speed = 0
+    return "You have arrived at your destination."
+  end
+  
+end
 
+class Pizza
+  
+  # Menu
+  # Type of pizza = Margherita, Deep dish, New York-style
+  # Specialization = Gluten-free, Vegan, Vegeterian
+  # Toppings = Pepperoni, Mushrooms, Onions, Sausage, Bacon, Extra cheese, Black olives, Green peppers, Pineapple, Spinach
+  
+  attr_reader :type
+  
+  def initialize(type)
+    @type = type
+    @toppings_list = []
+  end
 
-
+  def add_toppings(*toppings)
+    @toppings = *toppings
+    @toppings.each { |topping| @toppings_list << topping }
+  end
+  
+  def add_specialization(specialization)
+    specialization
+  end
+  
+end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+cheese_pizza = Pizza.new("New York-style")
+cheese_pizza.add_toppings("Cheese")
 
+player_one_car = Car.new("Beetle", "Yellow", cheese_pizza)
+player_one_car_directions = Directions.new(player_one_car)
+p player_one_car_directions.drive(0.25,25)
+p player_one_car_directions.turn_at_stop("right")
+p player_one_car_directions.drive(1.5,35)
+p player_one_car_directions.speed
+p player_one_car_directions.change_speed(-20)
+p player_one_car_directions.drive(0.25,15)
+p player_one_car_directions.turn_at_stop("left")
+p player_one_car_directions.drive(1.4,35)
+p player_one_car_directions.stop
+p player_one_car_directions.total_distance
+
+vegetarian_pizza = Pizza.new("Deep Dish")
+vegetarian_pizza.add_toppings("Mushrooms", "Onions", "Black olives", "Green peppers", "Spinach")
+vegetarian_pizza.add_specialization("Vegetarian")
+player_one_car.add_pizza(vegetarian_pizza)
+
+p player_one_car.pizza_count # => 2
+player_one_car.see_all_pizza # => cheese_pizza, vegetarian_pizza
+p player_one_car.get_pizza # => remove cheese_pizza
+
+p player_one_car.pizza_count # => 1
+player_one_car.see_all_pizza # => vegetarian_pizza
 
 
 
